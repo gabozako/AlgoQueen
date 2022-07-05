9e952cf89a0598c89dbd9d68bfaad369069de2d9
suy2on <tnwjdrms@naver.com>
2022-07-06 01:57:30 +0900

    # 7/5 : 2022카카오, 수정

--- suy2on/Programmers/2022Kakao/3.py
+++ suy2on/Programmers/2022Kakao/3.py
@@ -13,15 +13,13 @@ def solution(fees, records):
 
     for record in records:
         time, number, action = record.split()
-        parkingTimes[number].append(time_to_minute)
+        parkingTimes[number].append(time_to_minute(time))
 
     for car in sorted(parkingTimes.keys()):
-        print(parkingTimes)
         total = 0
         if len(parkingTimes[car]) % 2 == 1:  ## 마지막 출차기록 없으면 23:59
             parkingTimes[car].append(23 * 60 + 59)
         for i in range(0, len(parkingTimes[car]), 2):
-            print(parkingTimes[car][i + 1] - parkingTimes[car][i])
             total += parkingTimes[car][i + 1] - parkingTimes[car][i]
 
         if total <= fees[0]:  # 기본요금
@@ -33,4 +31,4 @@ def solution(fees, records):
     return answer
 
 
-solution([180, 5000, 10, 600], ["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"])
+print(solution([180, 5000, 10, 600], ["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]))
