0a275227711e794db39ff3740622229e52c83e10
suy2on <tnwjdrms@naver.com>
2022-07-25 04:14:58 +0900

    # 0725 : 2020카카오 2번, 재귀 문자열조작

--- suy2on/Programmers/2021kakao/6.py
+++ suy2on/Programmers/2021kakao/6.py
@@ -1,16 +1,18 @@
-import heapq
+import collections
+import itertools
 
 
 def solution(board, r, c):
-    cards = {}
+    cardNums = collections.defaultdict(list)
     idx = 1
     drc = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+    answer = float('inf')
 
     # 카드들 위치 체크
     for i in range(4):
         for j in range(4):
             if board[i][j]:
-                cards[(i, j)] = idx
+                cardNums[board[i][j]].append([i, j])
                 idx += 1
 
     def is_valid(i, j):
@@ -19,49 +21,49 @@ def solution(board, r, c):
         else:
             return False
 
-    pq = []
-    visited = [[[0] * (1 << (idx - 1)) for _ in range(4)] for _ in range(4)]
-    heapq.heappush(pq, (0, [r, c], 0, 0))
+    def bfs(start, finish, b):
+        queue = collections.deque()
+        queue.append([start, 0])
+        visited = [[0] * 4 for _ in range(4)]
+        visited[start[0]][start[1]] = 1
 
-    while pq:
-        cost, pos, card, target = heapq.heappop()
+        while queue:
+            pos, cost = queue.popleft()
+            # 방문
+            if pos == finish:
+                return cost
 
-        # 카드 다 찾은 경우
-        if card == 1 << idx - 1:
-            return cost
-
-        # 같은 카드찾은 상태로 같은 곳 방문
-        if visited[pos[0]][pos[1]][card]:
-            continue
-
-        visited[pos[0]][pos[1]][card] = 1
-
-        # 짝 찾은 경우
-        if target:
-            if board[pos[0]][pos[1]] == target:
-                card = card | 1 << cards[(pos[0], pos[1])]
-                cost += 1
-                target = 0
-
-        # 첫카드 찾은 경우
-        else:
-            if board[pos[0]][pos[1]]:
-                card = card | 1 << cards[(pos[0], pos[1])]
-                cost += 1
-                target = board[pos[0]][pos[1]]
-        # 방향키 4개 하나씩
-        for i in range(4):
-            nr = pos[0] + drc[i][0]
-            nc = pos[1] + drc[i][1]
-
-            if is_valid(nr, nc):
-                heapq.heappush(pq, (cost, [nr, nc], card, target))
-        # Ctrl 포함
-        for i in range(4):
-            for _ in range(4):
+            # 한칸씩
+            for i in range(4):
                 nr = pos[0] + drc[i][0]
                 nc = pos[1] + drc[i][1]
-                if board[nr, nc]:
+                if is_valid(nr, nc) and not visited[nr][nc]:
+                    visited[nr][nc] = 1
+                    queue.append([[nr, nc], cost + 1])
+            # CTRL
+            for i in range(4):
+                for j in range(4):
+                    nr = pos[0] + drc[i][0] * j
+                    nc = pos[1] + drc[i][1] * j
+                    # 끝에도달
+                    if not is_valid(nr, nc):
+                        visited[nr][nc] = 1
+                        queue.append([nr - drc[i][0], nc - drc[i][1], cost + 1])
+                        break
+                    # 카드발견
+                    if b[nr][nc]:
+                        visited[nr][nc] = 1
+                        queue.append([[nr, nc], cost + 1])
+                        break
+
+    for combi in itertools.permutations(cardNums.keys()):
+
+        q = collections.deque()
+        q.append([[r, c], 0, 0])
+
+        while q:
+            cur, i, cost = q.popleft()
+            bfs(combi[0]
 
 
 
